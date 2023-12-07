867ae92f72a18ab5663faa604dcb79d159e959e8
MGRichardson <142840558+MGRichardson@users.noreply.github.com>
2023-12-07 14:46:11 -0600

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,30 @@
+# Contact Manager Group Project #
+import random
+import os
+import math
+
+def main():
+    #main accepts no arguments
+    #calls menu() for options and choice
+    #use choice to call functions or exit program
+    
+    
+    
+def menu():
+    #menu accepts no arguments
+    #outputs list of options
+    #prompts for option chosen
+    #return choice
+    
+    
+    
+def add_contact():
+    #add contact accepts no arguments
+    #use file contacts.txt
+    #adds a name, address, phone, and email for each contact record
+    #ask if more than one contact is being made
+    
+    
+    
+def search_contact():
+    
