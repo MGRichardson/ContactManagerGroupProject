2c40c6e096ab57dbf85210fa46dd9c438151fa23
MGRichardson <142840558+MGRichardson@users.noreply.github.com>
2023-12-08 08:59:43 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -27,4 +27,33 @@ def add_contact():
     
     
 def search_contact():
+    #search contact accepts no arguments
+    #prompts for a contact tot search for
+    #search contacts.txt for contact
+    #when found, print
+    #if not found display error
+    
+    
+    
+def edit_contact():
+    #edit contact accepts no arguments
+    #prompts for a contact to find
+    #if not found display error
+    #when found prompt for what info to edit
+    #use temp to edit contacts.txt
+    
+    
+    
+def delete_contact():
+    #delete conatct accepts no arguments
+    #prompts for a contact to delete
+    #use temp to remove contact from contacts.txt
+    
+    
+    
+def display_contacts():
+    #display contacts accepts no arguments
+    #print all contacts in contacts.txt
+    
+    
     
