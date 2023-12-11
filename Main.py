3d6e318d0311c60eb3717763c5c605f0b771488b
MGRichardson <142840558+MGRichardson@users.noreply.github.com>
2023-12-11 14:00:26 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ def main():
     #calls menu() for options and choice
     #use choice to call functions or exit program
     
-    
+    pass
     
 def menu():
     #menu accepts no arguments
@@ -16,7 +16,7 @@ def menu():
     #prompts for option chosen
     #return choice
     
-    
+    pass
     
 def add_contact():
     #add contact accepts no arguments
@@ -24,7 +24,7 @@ def add_contact():
     #adds a name, address, phone, and email for each contact record
     #ask if more than one contact is being made
     
-    
+    pass
     
 def search_contact():
     #search contact accepts no arguments
@@ -33,7 +33,7 @@ def search_contact():
     #when found, print
     #if not found display error
     
-    
+    pass
     
 def edit_contact():
     #edit contact accepts no arguments
@@ -42,18 +42,18 @@ def edit_contact():
     #when found prompt for what info to edit
     #use temp to edit contacts.txt
     
-    
+    pass
     
 def delete_contact():
     #delete conatct accepts no arguments
     #prompts for a contact to delete
     #use temp to remove contact from contacts.txt
     
-    
+    pass
     
 def display_contacts():
     #display contacts accepts no arguments
     #print all contacts in contacts.txt
     
-    
+    pass
     
